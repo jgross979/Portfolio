cfde0e2894862a363f5a2ec9886d8160a3eb54de
Jared Gross <jagross@udel.edu>
2018-02-23 21:10:14 -0500

    First Draft

---
+++ app.js
@@ -0,0 +1,38 @@
+$(function(){
+// ------- Screen Resizing -----------//
+  $(function(){
+    if($(window).width()>999){
+      $('#container').css('width', $(window).width() - 185+'px');
+    }
+  });
+  $(window).resize(function(){
+    if($(window).width()>999 && !$('nav').hasClass('open')){
+      $('#container').css('width', $(window).width() - 185+'px');
+    }else{
+      $('#container').css('width', '100%');
+    }
+  })
+// ------- Name Appear -----------//
+  $('#heading').mouseover(function(){
+    if($(this).find('h2').hasClass('hidden')){
+      $(this).find('h2').toggleClass('hidden appear');
+    }
+  });
+
+// ------- Open/Close Nav -----------//
+  $('.nav-bars').click(function(e) {
+    $('nav').toggleClass('open close')
+    if($('nav').hasClass('open') && $(window).width()>999 || $(window).width() <= 400){
+      $('#container').css('width', '100%');
+    }else{
+      $('#container').css('width', $(window).width() - 185+'px');
+    }
+    e.stopPropagation();
+    });
+
+  $('.bot_pic').click(function(){
+    $(this).find('h2').toggleClass('hidden')
+    $(this).toggleClass('under')
+  })
+
+  });
